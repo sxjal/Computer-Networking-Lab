0b277023451018fd82eacf8e9255acbecd7c6029
SAJAL <72509355+sxjal@users.noreply.github.com>
2024-02-01 13:02:46 +0530

    Update client.py

--- Lab 3/Fragmentation/client.py
+++ Lab 3/Fragmentation/client.py
@@ -1,42 +1,27 @@
 import socket
 
-def send_message(sock, message):
-    # Split the message into chunks of 1024 bytes
-    chunks = [message[i:i+1024] for i in range(0, len(message), 1024)]
-    # Send each chunk separately
-    for chunk in chunks:
-        sock.send(chunk.encode())
-
+seq_data = {}
+ 
 def receive_message(sock):
-    # Receive the message in chunks of 1024 bytes
-    chunks = []
+     
     while True:
-        chunk = sock.recv(1024)
-        if not chunk:
-            break
-        chunks.append(chunk.decode())
-    # Concatenate the chunks to form the complete message
+        serverMsg = s.recv(1024).decode('UTF-8')
+        seqno,data = serverMsg.slpit(':')
+        print("Data Received ",seqno,":",data)
+        seq_data[int(seqno)] = data
+
     message = ''.join(chunks)
     return message
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+s.connect(('127.0.0.1', 3005))
+print("connected with server")
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-sock.connect(server_address)
-
-# Send a message to the server
 while True:
-    message = input()
-    send_message(sock, message)
-
-    if message == "break":
-        break
-    
-# Receive the response from the server
-response = receive_message(sock)
-print('Received {!r}'.format(response))
+    response = receive_message(s)
+    print('Received {!r}'.format(response))
 
 # Clean up the connection
-sock.close()
+s.close()
